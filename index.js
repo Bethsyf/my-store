6f83b45143f152dcd7d7cf2f74dda7af6c27374e
bethsyf <bethsyfalcon@gmail.com>
2024-04-14 20:49:18 -0500

    implementation separation of concerns with express.Router

--- index.js
+++ index.js
@@ -1,64 +1,19 @@
 const express = require('express');
-const faker = require('@faker-js/faker');
+const routerApi = require('./routes');
+
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hola mi server en expres');
+  res.send('Hola mi server en express');
 });
 
 app.get('/nueva-ruta', (req, res) => {
   res.send('Hola, soy una nueva ruta');
 });
 
-app.get('/products', (req, res) => {
-  const products = [];
-  const { size } = req.query;
-  const limit = size || 10;
-  for (let index = 0; index < limit; index++) {
-    products.push({
-      name: faker.commerce.productName(),
-      price: parseFloat(faker.commerce.price()),
-      image: faker.image.imageUrl(),
-    });
-  }
-  res.json(products);
-});
-
-app.get('/products/filter', (rq, res) => {
-  res.send('Yo soy un filter');
-});
-
-app.get('/products/:id', (req, res) => {
-  const { id } = req.params;
-  res.json({
-    id,
-    name: 'Product 2',
-    price: 2000,
-  });
-});
-
-app.get('/users', (req, res) => {
-  const { limit, offset } = req.query;
-  if (limit && offset) {
-    res.json({
-      limit,
-      offset,
-    });
-  } else {
-    res.send('No hay ningun parametro');
-  }
-});
-
-app.get('/categories/:categoryId/products/:productId', (req, res) => {
-  const { categoryId, productId } = req.params;
-  res.json({
-    categoryId,
-    productId,
-    price: 2000,
-  });
-});
+routerApi(app);
 
 app.listen(port, () => {
-  console.log('Mi port ' + port);
+  console.log('Mi port' + port);
 });
